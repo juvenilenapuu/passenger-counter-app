dae5b9f9afa0d47eb14b053fa3848f7c8354b8d3
juveNile <juvinilenapuu@gmail.com>
2023-07-13 17:13:38 +0300

    code instruction

--- script.js
+++ script.js
@@ -27,4 +27,7 @@
 // let humanRatio = myAge * myDogAge
 // document.write(humanRatio)
 
-
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
