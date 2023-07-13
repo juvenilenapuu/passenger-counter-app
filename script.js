fe9b679155613e39da51f8890da6c262737aad5a
Juvenilenapuu <napuujuvenile@gmail.com>
2023-07-13 16:56:52 +0300

    Passenger counter App

---
+++ script.js
@@ -0,0 +1,30 @@
+// document.getElementById("count").innerText=5
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+// // How to create variables:
+// var x;
+// let y;
+
+// // How to use variables:
+// x = 5;
+// y = 6;
+// let z = x + y;
+
+// let count = 5
+// console.log(count)
+
+// // 1. Create a variable, myAge, and set its value to your age
+// let myAge = 20
+
+// 2. Log the myAge variable to the console
+// console.log(myAge)
+
+// let myAge = 10
+// let myDogAge = 10
+// let humanRatio = myAge * myDogAge
+// document.write(humanRatio)
+
+
