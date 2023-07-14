b4c9cf37c2f872bb4b32c4ca5a835ffe7f03fe06
juveNile <napuujuvenile@gmail.com>
2023-07-14 14:13:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,15 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+
+
+let countEl = document.getElementById("count-el")
+
+// console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
