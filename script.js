55d58d842abb78887076205fc3edb734eb6494f6
Juvenilenapuu <napuujuvenile@gmail.com>
2023-07-28 12:24:10 +0300

    update

--- script.js
+++ script.js
@@ -27,4 +27,25 @@
 // let humanRatio = myAge * myDogAge
 // document.write(humanRatio)
 
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+ // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
 
