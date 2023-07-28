d96cc9225b619b47a1a4d25e5c4041211d30bd2d
@juveNile_ <napuujuvenile@gmail.com>
2023-07-28 15:48:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,49 +27,25 @@
 // let humanRatio = myAge * myDogAge
 // document.write(humanRatio)
 
-<<<<<<< HEAD
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     // 3. Render the variable in the saveEl using innerText
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
 
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
-
-
-function save() {
-    let countStr = count + " - "
-    saveEl.innerText += countStr
-    countEl.textContent = 0
-    count = 0
-}
-
-
-=======
-// intialize the count as 0
+    // intialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
->>>>>>> 505863bf16531ee1a72e145d65a379013413138a
+let saveEl = document.getElementById("save-el");
+        let countEl = document.getElementById("count-el");
+        let count = 0;
 
-
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
-
-
-function save() {
-    let countStr = count + " - "
-    saveEl.innerText += countStr
-    countEl.textContent = 0
-    count = 0
-}
+        function increment() {
+            count += 1;
+            countEl.innerText = count;
+        }
+        function save() {
+            let countStr = count + " - ";
+            saveEl.textContent += countStr;
+            countEl.textContent = 0;
+            count = 0;
+        }
